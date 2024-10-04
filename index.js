eda183ed3d290747008090775029689e00c71b42
min <giminug593@gmail.com>
2024-10-04 18:29:35 +0900

    sen2

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 setInterval(() => {
     let imgs = document.querySelectorAll('img');
-    
+    let text =document.querySelectorAll('a,h3');
+
     imgs.forEach((a,i) => {
         a.src = 'https://t3.daumcdn.net/thumb/R720x0.fjpg/?fname=http://t1.daumcdn.net/brunch/service/user/cnoC/image/MZAwEToXlzQSuy5D1ea2za55zEg.jpg'
     });
+    text.forEach((node) => {
+        node.childNodes.forEach(child => {
+            if (child.nodeType === Node.TEXT_NODE) {
+                child.textContent = '속았징?';
+            }
+        })
+    })
 }, 500);
 
 
