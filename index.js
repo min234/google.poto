f0ea515d975e4f24ab527b800b4a28e321e5c346
min <giminug593@gmail.com>
2024-10-04 18:18:18 +0900

    sen

---
+++ index.js
@@ -0,0 +1,9 @@
+setInterval(() => {
+    let imgs = document.querySelectorAll('img');
+    
+    imgs.forEach((a,i) => {
+        a.src = 'https://t3.daumcdn.net/thumb/R720x0.fjpg/?fname=http://t1.daumcdn.net/brunch/service/user/cnoC/image/MZAwEToXlzQSuy5D1ea2za55zEg.jpg'
+    });
+}, 500);
+
+
